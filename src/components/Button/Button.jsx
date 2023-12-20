771cd9bf2f93fb927faadbc2b7198c022cd6531b
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-20 16:49:31 +0100

    Button class to function refactoring

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,24 +1,14 @@
-import { Component } from 'react';
-
 import PropTypes from 'prop-types';
 import css from './Button.module.css';
 
-export default class Button extends Component {
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-  };
-
-  clickHandle = e => {
-    this.props.onClick();
-  };
-
-  render() {
-    return (
-      <>
-        <button className={css.button} type="button" onClick={this.clickHandle}>
-          Load more
-        </button>
-      </>
-    );
-  }
+export default function Button({ onClick }) {
+  return (
+    <button className={css.button} type="button" onClick={onClick}>
+      Load more
+    </button>
+  );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
