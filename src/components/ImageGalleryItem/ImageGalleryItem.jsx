0eb4d230bad4e6462955e11ea7677d67ea13c252
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-20 17:28:11 +0100

    ImageGalleryItem class to function refactoring

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,44 +1,24 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-export default class ImageGalleryItem extends Component {
-  static propTypes = {
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
-  };
-
-  handleClick = () => {
-    const { onClick, largeImageURL } = this.props;
+export default function ImageGalleryItem({
+  onClick,
+  largeImageURL,
+  webformatURL,
+}) {
+  const handleClick = () => {
     onClick(largeImageURL);
   };
 
-  render() {
-    const { webformatURL } = this.props;
-    return (
-      <li className={css.item} onClick={this.handleClick}>
-        <img className={css.image} src={webformatURL} alt="" loading="lazy" />
-      </li>
-    );
-  }
+  return (
+    <li className={css.item} onClick={handleClick}>
+      <img className={css.image} src={webformatURL} alt="" loading="lazy" />
+    </li>
+  );
 }
 
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import css from './ImageGalleryItem.module.css';
-
-// function ImageGalleryItem({ pictureURL }) {
-//   return (
-//     <li className={css.item}>
-//       <img className={css.image} src={pictureURL} alt="" />
-//     </li>
-//   );
-// }
-
-// ImageGalleryItem.propTypes = {
-//   pictureURL: PropTypes.string.isRequired,
-//   onClick: PropTypes.func.isRequired,
-// };
-
-// export default ImageGalleryItem;
+ImageGalleryItem.propTypes = {
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
